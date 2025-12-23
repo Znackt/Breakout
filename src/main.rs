ec4dbc54ec5dfa82533c8f471a9e200d836b14cd
Znackt <znackt@proton.me>
2025-12-23 16:04:33 +0530

    Added logic to create the player a rectangle and handling the moving and collision logic

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,54 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("BreakOut")]
+const PLAYER_SIZE: Vec2 = Vec2::from_array([150f32, 40f32]);
+const PLAYER_SPEED: f32 = 700f32;
 
+struct Player {
+    rect: Rect,
+}
+
+impl Player {
+    pub fn new() -> Self {
+        Self {
+            rect: Rect::new(
+                screen_width() * 0.5f32 - PLAYER_SIZE.x * 0.5f32,
+                screen_height() * 0.9f32,
+                PLAYER_SIZE.x,
+                PLAYER_SIZE.y,
+            ),
+        }
+    }
+
+    pub fn update(&mut self, dt: f32) {
+        let x_move = match (is_key_down(KeyCode::A), is_key_down(KeyCode::D)) {
+            (true, false) => -1f32,
+            (false, true) => 1f32,
+            _ => 0f32,
+        };
+        self.rect.x += x_move * dt * PLAYER_SPEED;
+
+        if self.rect.x < 0f32 {
+            self.rect.x = 0f32;
+        }
+
+        if self.rect.x > screen_width() - self.rect.w {
+            self.rect.x = screen_width() - self.rect.w;
+        }
+    }
+
+    pub fn draw(&self) {
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, BLUE);
+    }
+}
+
+#[macroquad::main("BreakOut")]
 async fn main() {
+    let mut player = Player::new();
+
     loop {
+        player.update(get_frame_time());
         clear_background(WHITE);
+        player.draw();
         next_frame().await;
     }
 }
